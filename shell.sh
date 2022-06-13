44ae032a231780bd040d395a640deeb42479e231
Nienkeeijsvogel <32362037+Nienkeeijsvogel@users.noreply.github.com>
2022-06-13 10:20:19 +0200

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,7 +1,7 @@
  sudo setenforce 0
  sudo sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
  swapoff -a
- sed -e '/swap/s/^/#/g' -i /etc/fstab
+ sudo sed -e '/swap/s/^/#/g' -i /etc/fstab
  sudo systemctl stop firewalld
  lsmod | grep br_netfilter
  modprobe br_netfilter
