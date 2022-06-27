e8646f00ee0b947baa5bfd9c6b05443ec568a108
Nienkeeijsvogel <32362037+Nienkeeijsvogel@users.noreply.github.com>
2022-06-27 15:08:56 +0200

    Update startzk.sh

--- zookeeper-kafka/startzk.sh
+++ zookeeper-kafka/startzk.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-singularity run --writable-tmpfs --env-file env1 --hostname zookeeper-1 docker://confluentinc/cp-zookeeper:3.3.0-1
+singularity run --writable-tmpfs --env ZOOKEEPER_CLIENT_PORT=12181 --hostname zookeeper-1 docker://confluentinc/cp-zookeeper:3.3.0-1
