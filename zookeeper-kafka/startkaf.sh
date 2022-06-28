fd265ddd06f6a63e35d4fab8e43d23720a0c10ed
Nienkeeijsvogel <32362037+Nienkeeijsvogel@users.noreply.github.com>
2022-06-28 08:38:10 +0200

    Update startkaf.sh

--- zookeeper-kafka/startkaf.sh
+++ zookeeper-kafka/startkaf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo singularity run --hostname kafka-1 --env KAFKA_ZOOKEEPER_CONNECT=localhost:12181 --env KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:19092 --writable-tmpfs docker://confluentinc/cp-kafka:4.1.2-2
+singularity run --hostname kafka-1 --env KAFKA_ZOOKEEPER_CONNECT=localhost:12181 --env KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:19092 --writable-tmpfs docker://confluentinc/cp-kafka:4.1.2-2
